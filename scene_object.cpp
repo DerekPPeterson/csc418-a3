0b7091f9fa3de5e47eb388917a48c36b8d5c210c
Derek Peterson <derekpeterson.dpp@gmail.com>
2014-11-19 23:54:25 -0500

    somewhat working sphere

--- scene_object.cpp
+++ scene_object.cpp
@@ -60,6 +60,12 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
             return false;
         }
     }
+    
+    // check if closer intersection
+    if (!ray.intersection.none && 
+            (obRay.intersection.t_value > ray.intersection.t_value)) {
+        return false;
+    }
 
     // convert back to world space
     ray.intersection.none = false;
@@ -81,7 +87,51 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	//
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
-	
-	return false;
+    
+    // Ray in object space
+    Ray3D obRay = Ray3D();
+    obRay.origin = worldToModel * ray.origin;
+    obRay.dir    = worldToModel * ray.dir;
+
+
+    Point3D origin = Point3D(0, 0, 0);
+    Vector3D rayOrginVec = obRay.origin - origin;
+
+    // Parameters TODO see wikipedia
+    double a = obRay.dir.dot(obRay.dir);
+    double b = 2 * obRay.dir.dot(rayOrginVec);
+    double c = rayOrginVec.dot(rayOrginVec) - 1;
+
+    // determin if intersection
+    double det = b * b - 4 * a * c;
+
+    if (det < 0) {
+        // no intersection
+        return false;
+    } else {
+        // one or 2 intersections
+        double t_val_norm1 = (-b + sqrt(det)) / 2 / a;
+        double t_val_norm2 = (-b - sqrt(det)) / 2 / a;
+
+        obRay.intersection.t_value = t_val_norm2;
+        
+        // check if closer intersection
+        if (!ray.intersection.none && 
+                obRay.intersection.t_value > ray.intersection.t_value) {
+        }
+
+        obRay.intersection.point = 
+            obRay.origin + obRay.intersection.t_value * obRay.dir;
+
+        Vector3D normal = obRay.intersection.point - origin;
+
+        
+        ray.intersection.none = false;
+        ray.intersection.t_value = obRay.intersection.t_value;
+        ray.intersection.point = modelToWorld * obRay.intersection.point;
+        ray.intersection.normal = transNorm(worldToModel, normal);
+
+        return true;
+    }
 }
 
