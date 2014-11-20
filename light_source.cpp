831009ea5d2f8a4d4aa73c88e163fbb020caeba0
Derek Peterson <derekepterson.dpp@gmail.com>
2014-11-20 01:43:24 -0500

    working specular

--- light_source.cpp
+++ light_source.cpp
@@ -24,6 +24,9 @@ void PointLight::shade( Ray3D& ray ) {
 	// before this function.  
 
     Colour col(0, 0, 0);
+    Material* mat = ray.intersection.mat;
+
+    Colour ambient = _col_ambient * ray.intersection.mat->ambient;
 
     // Ray from intersection point to light source
     Vector3D lightDir = get_position() - ray.intersection.point;
@@ -31,10 +34,19 @@ void PointLight::shade( Ray3D& ray ) {
     normal.normalize();
     lightDir.normalize();
 
-    // diffuse
     Colour diffuse = max(0.0, normal.dot(lightDir)) * ray.intersection.mat->diffuse * _col_diffuse;
     col = col + diffuse;
 
+    // Perfect reflection dir
+    Vector3D reflectDir = 2 * (normal.dot(lightDir)) * normal - lightDir;
+    Point3D origin(0, 0, 0);
+    Vector3D viewDir = - ray.dir;
+    viewDir.normalize();
+    Colour specular = pow(max(reflectDir.dot(viewDir), 0.0), mat->specular_exp)
+            * _col_specular * ray.intersection.mat->specular ;
+    col = col + specular;
+
+    col.clamp();
     ray.col = col;
 
 }
